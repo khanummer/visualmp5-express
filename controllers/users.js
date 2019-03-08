a1c7366f5f6e08196d94658f0c93ae78315cd189
Ummer Khan <khanummergit@gmail.com>
2019-03-08 13:52:36 -0800

    login and register working

--- controllers/users.js
+++ controllers/users.js
@@ -3,15 +3,11 @@ const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
-// router.get('/', (req, res) => {
-//     res.send('users route is hitting')
-// })
 
+// get all Users route
 router.get('/', async (req, res) => {
-    // req.body this is from the fetch request
       try  {
       const allUsers = await User.find();
-    //   console.log(allUsers)
         res.json({
         status: 200,
         data: allUsers
@@ -21,7 +17,7 @@ router.get('/', async (req, res) => {
     }
   });
 
-
+// register (create user) route
 router.post('/', async (req, res) => {
     let crypted = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
     req.body.password = crypted;
@@ -37,5 +33,58 @@ router.post('/', async (req, res) => {
         res.send(err);
     }
 });
+
+// login route
+router.post('/login', async (req, res) => {
+    try {
+        //find logged in user //getting username from req.body (username was attached via form and kept in req.body)
+        const loggedUser = await User.findOne({ username: req.body.username });
+        //if user exists
+        if (loggedUser) {
+            //check if the passwords match, if they do, redirect to page, if not, keep on splash page with message
+            //calling loggedUser's password from schema and comparing it to the form attached to req.body
+            if (bcrypt.compareSync(req.body.password, loggedUser.password) && req.body.username === loggedUser.username) {
+                //once find user
+                //have to set session.message to empty string
+                res.json({loggedUser, isLoggedIn: true});
+            } else {
+                res.json({ isLoggedIn: false});
+            }
+        } else {
+            res.json({
+                status: 200,
+                data: 'login successful',
+                user: loggedUser
+            });
+        }
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+// logout route
+router.get('/logout', async(req, res) => {
+    req.logout()
+    res.json({
+        user: req.user
+    });
+});
+
+
+// show route
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const foundUser = await User.findById(req.params.id);
+//         res.json({
+//             status: 200,
+//             data: foundUser
+//         });
+//     } catch (err) {
+//         console.log(err);
+//         res.send(err);
+//     }
+// });
+
+
 
 module.exports = router
