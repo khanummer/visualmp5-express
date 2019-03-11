ed44e65100d8899fafd6eede8a3313017f693cac
Ummer Khan <khanummergit@gmail.com>
2019-03-11 15:52:52 -0700

    adds lastFM search

--- controllers/api.js
+++ controllers/api.js
@@ -1,72 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-// const axios = require('axios');
+const axios = require('axios');
 
-// app.get('/soundcloud', async (req, res) => {
-//     axios.get(``)
-//     .then((response) => {
-//         console.log(response)
-//         // return res.json(response.data)
-//     })
-//     .catch(function (error) {
-//         console.log(error)
-//     });
-// });
-
-// spotify API 
-var SpotifyWebApi = require('spotify-web-api-node');
-
-var spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: 'localhost:3000'
-});
-
-spotifyApi.setAccessToken(process.env.SPOTIFY_ACCESS_TOKEN);
-// ------------
-// spotifyApi.getTrack('0pBcI8Gf1Oe3ziIbE9tBsX')
-//   .then(function(data) {
-//     console.log('get nipsey hussle', data.body);
-//   }, function(err) {
-//     console.error(err);
-//   });
-
-// spotifyApi.searchTracks('love hurts')
-//   .then(function(data) {
-//     console.log('Search by "love hurts"', data.body.tracks.items);
-//   }, function(err) {
-//     console.error(err);
-//   });
+const lastFmApiKey = process.env.LASTFM_API_KEY
 
 
-// search for tracks
-//   app.get(`/spotify/search/${req.params.query}`, async (req, res) => {
-//     axios.get(`https://api.spotify.com/v1/search?q=${req.params.query}&type=track`, {
-//       headers: {
-//         Authorization: process.env.SPOTIFY_ACCESS_TOKEN
-//       }
-//     })
-//     .then(function (response) {
-//       console.log(response.data)
-//         return res.json(response.data)
-//     })
-//     .catch(function (error) {
-//         console.log(error)
-//     });
-//   });
 
-    // axios.get(`https://api.spotify.com/v1/search?q=pokemon&type=track`, {
-    //   headers: {
-    //     Authorization: process.env.SPOTIFY_ACCESS_TOKEN
-    //   }
-    // })
-    // .then(function (response) {
-    //   console.log(response.data)
-    //     // return res.json(response.data)
-    // })
-    // .catch(function (error) {
-    //     console.log(error)
-    // });
+router.get(`/lastFM/search/:search`, async (req, res) => {
+    axios.get(`http://ws.audioscrobbler.com/2.0/?method=track.search&track=${req.params.search}&api_key=${lastFmApiKey}&format=json`)
+    .then(function (response) {
+      console.log(response.data)
+        return res.json(response.data)
+    })
+    .catch(function (error) {
+        console.log(error)
+    });
+  });
+
 
 
 
