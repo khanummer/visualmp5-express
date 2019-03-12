2b1a4ea3e4a2e7f1310d79d8f450ce65f0383b6c
Ummer Khan <khanummergit@gmail.com>
2019-03-12 11:29:20 -0700

    adds giphy search query

--- controllers/api.js
+++ controllers/api.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const axios = require('axios');
 
 const lastFmApiKey = process.env.LASTFM_API_KEY
-
+const giphyApiKey = process.env.GIPHY_API_KEY
 
 
 router.get(`/lastFM/search/:search`, async (req, res) => {
     axios.get(`http://ws.audioscrobbler.com/2.0/?method=track.search&track=${req.params.search}&api_key=${lastFmApiKey}&format=json`)
     .then(function (response) {
-      console.log(response.data)
+      // console.log(response.data)
         return res.json(response.data)
     })
     .catch(function (error) {
@@ -18,7 +18,15 @@ router.get(`/lastFM/search/:search`, async (req, res) => {
     });
   });
 
-
+router.get(`/giphy/search/:search`, async (req,res) => {
+  axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&q=${req.params.search}&limit=25&offset=0&rating=G&lang=en`)
+  .then(function (response) {
+    return res.json(response.data) 
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+})
 
 
 module.exports = router;
--- models/mashup.js
+++ models/mashup.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // default zero
 const mashupSchema = new mongoose.Schema({
     userId: String,
-    spotifySong: String,
+    song: {name: String, artist: String, url: String, userId: String},
     Image: String,
     Video: String,
     GIF: String,
