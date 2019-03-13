923c2870c35afa411dc78067fd7055f4b96749d3
Ummer Khan <khanummergit@gmail.com>
2019-03-13 15:11:22 -0700

    add env

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const database = process.env.MONGODB_URI
 
 // create our db and connect
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(database, {
    useNewUrlParser: true, 
    useCreateIndex: true,
    useFindAndModify: false  
