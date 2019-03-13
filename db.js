77f3c60696f08f1dff11b92aa9e061cd0f7d7d5b
Ummer Khan <khanummergit@gmail.com>
2019-03-13 15:04:20 -0700

    heroku deployment

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // create our db and connect
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
    useNewUrlParser: true, 
    useCreateIndex: true,
    useFindAndModify: false  
